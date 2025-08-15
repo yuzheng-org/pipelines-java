6b4503d891d60a7dc120a1d9d7620ea451d0a1eb
yuzhengchua <yuzheng.chua@sonarsource.com>
2025-08-15 16:52:59 +0800

    trigger PR

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,7 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am not covered");
+            System.out.println("I am actually covered");
             return;
         }
 
